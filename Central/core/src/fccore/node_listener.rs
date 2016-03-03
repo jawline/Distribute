77a16479188e68fe545b34eb2ede363982db5f8b
Blake Loring <blake_l@parsed.uk>
2016-03-03 10:33:56 +0000

    NodeListener listener

--- Central/core/src/fccore/node_listener.rs
+++ Central/core/src/fccore/node_listener.rs
@@ -1,2 +1,24 @@
-pub fn node_listener(port: usize, core: &Arc<Mutex<Core>>) {
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use fccore::node::Node;
+
+pub fn node_listener(address: &str, core: Arc<Mutex<Core>>) {
+
+	let listener = TcpListener::bind(address).unwrap();
+
+	// accept connections and process them, spawning a new thread for each one
+	for stream in listener.incoming() {
+	    match stream {
+	        Ok(stream) => {
+	            thread::spawn(move|| {
+	                // connection succeeded
+	                handle_client(stream)
+	            });
+	        }
+	        Err(e) => { /* connection failed */ }
+	    }
+	}
+
+	// close the socket server
+	drop(listener);
 }
